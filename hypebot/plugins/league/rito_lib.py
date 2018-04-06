d393c43d6b04d177782a72049e8c33facb0b0a89
Zak Wise <zaw@google.com>
2018-04-06 11:37:54 -0400

    Rename ListRankedMatches to ListMatches. Replace ListRecentMatches with ListMatches, since the former is being turned down on 2018/04/27.

--- hypebot/plugins/league/rito_lib.py
+++ hypebot/plugins/league/rito_lib.py
@@ -150,8 +150,8 @@ class RitoLib(object):
 
   def ListRecentMatches(self, region, account_id):
     return self._CallApi(
-        self._match_service.ListRecentMatches,
-        match_pb2.ListRecentMatchesRequest(account_id=account_id),
+        self._match_service.ListMatches,
+        match_pb2.ListMatchesRequest(account_id=account_id, end_index=20),
         region)
 
   def GetMatch(self, region, game_id):
