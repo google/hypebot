11bc63785d3a2a6e1e1d6f0f8955a71454ab15a4
HypeBot Contributor <noreply@google.com>
2019-10-01 12:40:58 -0600

    Re-enable international schedules for worlds.
    PiperOrigin-RevId: 272258816

--- hypebot/plugins/league/esports_lib.py
+++ hypebot/plugins/league/esports_lib.py
@@ -831,9 +831,9 @@ class EsportsLib(object):
     self._providers = [
         BattlefyProvider(self._proxy, self._rito, '5d58669ea837004f58b095ad',
                          'CEA', stats_enabled=True),
-        # RitoProvider(self._proxy, 'IN', 'worlds',
-        #              aliases=['International', 'Worlds'],
-        #              stats_enabled=False),
+        RitoProvider(self._proxy, 'IN', 'worlds',
+                     aliases=['International', 'Worlds'],
+                     stats_enabled=True),
         RitoProvider(self._proxy, 'NA', 'lcs', aliases=['North America'],
                      stats_enabled=True),
         RitoProvider(self._proxy, 'EU', 'lec', aliases=['Europe'],
